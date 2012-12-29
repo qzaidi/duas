3bddad8af73051c799e3d9fb077c78e0dfae82d4
Qasim Zaidi <qasim@zaidi.me>
2012-12-29 13:50:43 +0530

    fix social sharing

--- routes/dates.js
+++ routes/dates.js
@@ -1,10 +1,32 @@
 "use strict";
 
+// TODO: this will come from DB.
+var keydates = {
+  'arbaeen': {
+    page: {
+      'title': 'Arbaeen 2013',
+      'description': 'Arbaeen of the martyrs of Karbala',
+      'image': '/img/arbaeen.jpg'
+    },
+  },
+  'sakina' : {
+    page: {
+      'title': 'Martyrdom of Sakina, 17th of Safar',
+      'description': 'Imam Husain\'s beloved daughter died of cold and starvation in Yazeed\'s Prison',
+      'image': '/img/sakina.jpg'
+    }
+  }
+};
+
 var dates = {
   page: function(req,res,next) {
-    var page = {};
-    page.image = 'http:// ' + req.host + '/img/' + req.params.page + '.jpg' ;
-    res.render('dates/' + req.params.page, { page: page });
+    var info = keydates[req.params.page];
+    if (!info) {
+      return next(new Error('Not Found'));
+    }
+
+    info.page.image = 'http://' + req.host + info.page.image;
+    res.render('dates/' + req.params.page, { page: info.page });
   }
 };
 
