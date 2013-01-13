003d378b97308fe89b852d9909815ab767a0f254
Qasim Zaidi <qasim@zaidi.me>
2013-01-13 20:25:43 +0530

    rich snippets for events

--- routes/events.js
+++ routes/events.js
@@ -3,8 +3,25 @@
 var db = require('../model/db');
 var hijri = require('../lib/hijri');
 
+function get_nth_suffix(date) {
+  switch (date) {
+    case 1:
+    case 21:
+    case 31:
+      return 'st';
+    case 2:
+    case 22:
+      return 'nd';
+    case 3:
+    case 23:
+      return 'rd';
+    default:
+      return 'th';
+  }
+}
 
 var events = {
+  
   page: function(req,res,next) {
     var urlkey = req.params.page;
     var sql = 'select * from events where urlkey = "' + urlkey + '"';
@@ -19,7 +36,7 @@ var events = {
       page.description = ev.metadesc;
       ev.crdate = hijri.getGregorianDate({ day: ev.hijridate, month: ev.hijrimonth -1 });
       ev.month = hijri.months[ev.hijrimonth - 1];
-      res.render('dates/index', { page: page, event: ev });
+      res.render('dates/index', { page: page, event: ev, suffix: get_nth_suffix });
     });
   }
 };
