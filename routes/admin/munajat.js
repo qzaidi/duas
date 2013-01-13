253b508b9bd437b8d606e365e58701cfafd759f5
Qasim Zaidi <qasim@zaidi.me>
2013-01-13 17:19:06 +0530

    added urdu for rajeen

--- routes/admin/munajat.js
+++ routes/admin/munajat.js
@@ -25,14 +25,12 @@ var munajat = {
   },
 
   update: function(req,res,next) {
-    var sql = 'update ' + req.params.name + ' set urdu = "' + req.body.urdu + '", cue = "' +  req.body.cue + '" where rowid=' + req.body.rowid; 
-    console.log(sql);
-    db.run(sql, function(err,st) {
+    db.update(req.params.name, req.body, [ 'rowid' ], function(err,st) {
       var resp = { "Result": "OK" };
       if (err) {
         console.log(err);
         resp.Result = 'ERROR';
-        resp.message = err.message;
+        resp.Message = err.message;
       } 
       res.json(resp);
     });
