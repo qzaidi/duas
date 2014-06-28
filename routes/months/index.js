ba0fd9f1a2643275de120d2cbbd60e4863f4786b
Qasim Zaidi <qasim@zaidi.me>
2014-06-28 12:17:06 +0530

    add ramadhan content

--- routes/months/index.js
+++ routes/months/index.js
@@ -4,4 +4,6 @@ var ramadhan = require('./ramadhan');
 
 module.exports = function(app) {
   app.get('/ramadhan/daily/:day', ramadhan.daily);
+  app.get('/ramadhan/daily', ramadhan.dayindex);
+  app.get('/duas/ramadhan', ramadhan.index);
 };
--- routes/months/ramadhan.js
+++ routes/months/ramadhan.js
@@ -5,13 +5,22 @@ var hijri = require('../../lib/hijri');
 
 var ramadhan =  {
 
+
+  index: function(req,res,next) {
+    res.render('months/ramadhan/index');
+  },
+
+  dayindex: function(req,res,next) {
+    res.render('months/ramadhan/dayindex');
+  },
+
   daily: function(req,res,next) {
     db.get('select * from ramadhandaily where day = ' + req.params.day, function(err,row) {
       if (err) {
         return next(err);
       }
       row.page = { title: 'Daily dua for ' + req.params.day + hijri.get_nth_suffix(req.params.day) + ' Ramadhan ' , description: row.english };
-      res.render('months/ramadhan', row);
+      res.render('months/ramadhan/daily', row);
     });
   }
 
