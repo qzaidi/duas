471427a453c40020fa2a263c90f1edcd413ebc01
Qasim Zaidi <qasim@zaidi.me>
2015-01-03 18:39:59 +0530

    add sermon2, update cover & hadees kisa urdu

--- routes/verseofday.js
+++ routes/verseofday.js
@@ -19,7 +19,9 @@ var vod = {
   get: function(req,res,next) {
     var l = vod.verses.length;
     var i = 18 ; // ((Math.random()*l)|0)%l;
-    var selected = vod.verses[i];
+    var selected = vod.verses.filter(function(v) {
+      return v.selected;
+    })[0];
     req.verseofday = { arabic: selected.arabic, href: selected.href };
     
     req.verseofday.background = selected.image || 'quranlight.jpg';
